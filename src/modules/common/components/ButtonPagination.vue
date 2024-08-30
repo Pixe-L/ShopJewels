2a67bb56ecadfdcbf6c4ceb5d05dc03cccaf30f7
Arturo <tutospixel@hotmail.com>
2024-08-29 22:43:23 -0600

    added pagination in the app

--- src/modules/common/components/ButtonPagination.vue
+++ src/modules/common/components/ButtonPagination.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="flex justify-center py-10 text-slate-100 bg-gray-100 dark:bg-slate-900 gap-5">
     <button
-      class="flex items-center space-x-1.5 bg-blue-500 disabled:bg-gray-300 disabled:text-slate-800 rounded py-1 px-5 hover:bg-blue-600"
+      :disabled="page === 1"
+      @click="$router.push({ query: { page: page - 1 } })"
+      class="flex items-center space-x-1.5 bg-blue-500 disabled:bg-gray-300 disabled:text-slate-800 rounded py-1 px-5 hover:bg-blue-600 transition-all"
     >
       <arrow-left-icon />
-      <span>Previous</span>
+      <span>Back</span>
     </button>
     <button
-      class="flex items-center space-x-1.5 bg-blue-500 disabled:bg-gray-300 rounded py-1 px-5 hover:bg-blue-600"
+      :disabled="hasMoreData"
+      @click="$router.push({ query: { page: page + 1 } })"
+      class="flex items-center space-x-1.5 bg-blue-500 disabled:bg-gray-300 rounded py-1 px-5 hover:bg-blue-600 transition-all"
     >
       <span>Next</span>
       <arrow-right-icon />
@@ -18,6 +22,12 @@
 <script setup lang="ts">
 import ArrowLeftIcon from '@/icons/ArrowLeftIcon.vue';
 import ArrowRightIcon from '@/icons/ArrowRightIcon.vue';
+
+interface Props {
+  page: number;
+  hasMoreData: boolean;
+}
+defineProps<Props>();
 </script>
 
 <style scoped></style>
